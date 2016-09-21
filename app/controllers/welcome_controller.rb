ada6480c447c68c5700f14e546ed81816e12d442
Taekmin Kim <tantara.tm@gmail.com>
2016-09-21 21:57:41 +0900

    mobile에 webview랑 연동

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+		if user_signed_in?
+			redirect_to courses_path
+			return
+		end
+
     @msg = "Hello DeepCheck!"
 
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,26 @@
 Rails.application.routes.draw do
 	devise_for :users
-	devise_scope :user do
-		#authenticated :user do
-			resources :courses, :only => [:index, :create, :show, :update, :delete] do
-				post "/join" => "courses#join", :on => :member
-				delete "/leave" => "courses#leave", :on => :member
+	authenticated :user do
+		root "welcome#index"
+		resources :courses, :only => [:index, :create, :show, :update, :delete] do
+			post "/join" => "courses#join", :on => :member
+			delete "/leave" => "courses#leave", :on => :member
 
-				resources :posts, :only => [:index, :create, :show, :update, :delete] do
-					resources :photos, :only => [:index, :create, :update, :delete] do
-						post '/tag' => "photos#add_tag", :on => :member
-						put '/tag/:tag_id' => "photos#edit_tag", :on => :member
-						delete '/tag/:tag_id' => "photos#remove_tag", :on => :member
-					end
-					resources :comments, :only => [:index, :create, :update, :delete]
+			resources :posts, :only => [:index, :create, :show, :update, :delete] do
+				resources :photos, :only => [:index, :create, :update, :delete] do
+					post '/tag' => "photos#add_tag", :on => :member
+					put '/tag/:tag_id' => "photos#edit_tag", :on => :member
+					delete '/tag/:tag_id' => "photos#remove_tag", :on => :member
 				end
+				resources :comments, :only => [:index, :create, :update, :delete]
 			end
-			resources :users
-		#end
-
-		unauthenticated :user do
-			root "welcome#index"
-			resources :users, :only => [:create]
 		end
+		resources :users
+	end
+
+	unauthenticated :user do
+		root "welcome#index"
+		resources :users, :only => [:create]
 	end
 
 	devise_for :admins
