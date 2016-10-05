d6c1ccb44cb4ff4a20b46cb496c73684e7ba5fcd
MaybeS <maytryark@gmail.com>
2016-10-05 19:49:52 +0900

    update header margin and remove unused modal code

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,42 +1,34 @@
 $(document).ready(function() {
   var grids = document.getElementsByClassName('face-grid');
+  var offsetY = 40;
+  var offsetX = -24;
+
   for (var i = 0; i < grids.length; ++i) {
     $('#' + grids[i].getAttribute('id'))
       .css('left', grids[i].getAttribute('data-x') + 'px')
       .css('top', grids[i].getAttribute('data-y') + 'px');
   }
-
+  
   $('.photo').on("click", function(e) {
-    $('.face-select').css('display', "block");
-    var offsetY = 40;
-    var offsetX = -24;
-
     var pos = $(this).offset();
-    console.log(e.pageX);
-    console.log(e.pageY);
     var x = e.pageX - pos.left + offsetX;
     var y = e.pageY - pos.top + offsetY;
+    var pid = $('.face-select').data('photo-id');
+    var uid = $('.face-select').data('user-id');
+
     $('.face-select')
+      .css('display', "block")
       .css('left', x)
       .css('top', y);
-  });
 
-  $('.face-select').on("click", function(e) {
-    var pid = $(this).data('photo-id');
-    var uid = $(this).data('user-id');
-    if ($('.face-select').css('display') == "block" )
-    {
-      var x = $(this).css('left').slice(0, -2) * 1;
-      var y = $(this).css('top').slice(0, -2) * 1;
-      $.ajax({ 
-        type: 'PATCH',
-        url: window.location.href + '/photos/' + pid,
-        dataType: 'text',
-        data: { x: x, y: y, uid : uid },
-        success: function(result){
-          location.reload();
-      }}); 
-    }
+    $.ajax({ 
+      type: 'PATCH',
+      url: window.location.href + '/photos/' + pid,
+      dataType: 'text',
+      data: { x: x, y: y, uid : uid },
+      success: function(result){
+        location.reload();
+    }}); 
   });
 });
 
