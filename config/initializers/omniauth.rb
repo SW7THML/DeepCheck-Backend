e8887e858b06c85b24d7a9f57a949bdd9e1fd91d
MaybeS <maytryark@gmail.com>
2016-10-05 19:09:16 +0900

    update key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook, '1038966506229104', 'd218f6844561d97f593f0f8877a73d62'
+
+  if Rails.env.production?
+    # Production
+    provider :facebook, '1038966506229104', 'd218f6844561d97f593f0f8877a73d62'
+  else
+    # Dev
+    provider :facebook, '535490729982998', '55f760d12b532a636b7a226c224825d4'
+  end
 end
