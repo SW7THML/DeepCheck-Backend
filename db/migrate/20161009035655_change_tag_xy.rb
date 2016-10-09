969c2e7b6ab657323c9356fb016acaa5e79e908e
SeulA Kim <tmfdk333@daum.net>
2016-10-09 14:57:34 +0900

    fix irreservable db migration error

--- db/migrate/20161009035655_change_tag_xy.rb
+++ db/migrate/20161009035655_change_tag_xy.rb
@@ -1,6 +1,11 @@
 class ChangeTagXy < ActiveRecord::Migration[5.0]
-  def change
-    change_column :tagged_users, :x,  :float
-    change_column :tagged_users, :y,  :float
+  def up
+    change_column :tagged_users, :x, :float
+    change_column :tagged_users, :y, :float
+  end
+
+  def down
+    change_column :tagged_users, :x, :integer
+    change_column :tagged_users, :y, :integer
   end
 end
