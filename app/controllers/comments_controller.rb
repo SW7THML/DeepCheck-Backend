d64eed27b571aaceadc3d268091a6ee7fcb58a82
SeulA Kim <tmfdk333@daum.net>
2016-10-09 16:49:05 +0900

    Add Comment Validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,11 @@ class CommentsController < ApplicationController
     @post = @course.posts.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
 
-    @comment.save!
-    redirect_to course_post_path(@course, @post)
+    if @comment.save
+      redirect_to course_post_path(@course, @post)
+    else
+      redirect_to course_post_path(@course, @post), alert: "최소 한 글자 이상의 댓글을 입력해주세요."
+    end
   end
 
   def destroy
