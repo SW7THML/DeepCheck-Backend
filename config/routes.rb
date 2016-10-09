2dbbc3481cce8cdd066604c5f2204a84ac6287ca
SeulA Kim <tmfdk333@daum.net>
2016-10-09 13:50:47 +0900

    adjust indents

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,16 @@
 Rails.application.routes.draw do
-  # devise_for :admins
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "courses#index"
 
-	resources :courses, only: [:new, :create, :show, :index, :edit, :update] do
+  resources :courses, only: [:new, :create, :show, :index, :edit, :update] do
     resources :posts, :only => [:index, :show, :create] do 
       resources :photos, :comments, :only => [:index, :show, :create, :update, :destroy]
     end
   end
 
-  #member do
-  #  namespace :link, module: nil do
-  #    get 'ios' => "ios_link#show"
-  #  end
-  #end
-
-	resources :users
-	resources :comments
+  resources :users
+  resources :comments
 
   namespace :api, module: nil do
     resources :courses, only: [:show]
