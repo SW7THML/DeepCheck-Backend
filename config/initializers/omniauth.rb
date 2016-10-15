0aa978eba451c31afd602ad58d31657605772cb0
MaybeS <maytryark@gmail.com>
2016-10-16 05:49:47 +0900

    update key(dev-facebook-app) and button border

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, '1038966506229104', 'd218f6844561d97f593f0f8877a73d62'
   else
     # Dev
-    provider :facebook, '1038966506229104', 'd218f6844561d97f593f0f8877a73d62'
+    provider :facebook, '550795318452539', 'd64fdd0894bb2c06956811e9b9b72ce1'
   end
 end
