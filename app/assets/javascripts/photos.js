41a0697e2605af6ee1ff75198915d51e03314f22
SeulA Kim <tmfdk333@daum.net>
2016-10-23 12:37:53 +0900

    image height

---
+++ app/assets/javascripts/photos.js
@@ -0,0 +1,9 @@
+// $(function(){
+// 	var width = $('html').css('width');
+// 	$('img.photo').height(width);
+// });
+
+function fit_photo_width(){
+	var width = $('html').css('width');
+	$('img.photo').height(width);
+}
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -14,6 +14,26 @@ class PhotoUploader < CarrierWave::Uploader::Base
     self.model.process
   end
 
+  version :thumbnail do
+    process :resize_to_fill => [600, 600]
+    # process :resize_and_crop => 300
+  end
+
+  def resize_and_crop(size)  
+    manipulate! do |image|                 
+      if image[:width] < image[:height]
+        remove = ((image[:height] - image[:width])/2).round 
+        image.shave("0x#{remove}") 
+      elsif image[:width] > image[:height] 
+        remove = ((image[:width] - image[:height])/2).round
+        image.shave("#{remove}x0")
+      end
+      image.resize("#{size}x#{size}")
+      image
+    end
+  end
+
+
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
