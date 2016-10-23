30562d18c021610d316b91b2eccfc2946190819e
Taekmin Kim <tantara.tm@gmail.com>
2016-10-23 19:31:13 +0900

    set as now()

--- db/migrate/20161023102137_change_default_value_to_posts.rb
+++ db/migrate/20161023102137_change_default_value_to_posts.rb
@@ -1,6 +1,6 @@
 class ChangeDefaultValueToPosts < ActiveRecord::Migration[5.0]
   def change
     change_column :posts, :taken_at, :date
-    change_column_default :posts, :taken_at, ''
+    change_column_default :posts, :taken_at, 'now()'
   end
 end
